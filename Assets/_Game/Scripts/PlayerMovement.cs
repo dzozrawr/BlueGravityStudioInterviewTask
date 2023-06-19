ca770414ad8277b6f4d6984eaa0ad9eaf3550c14
dzozrawr <jovan_96@live.ru>
2023-06-19 14:45:45 +0200

    finished movement

---
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
