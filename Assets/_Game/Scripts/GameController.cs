cda393dae11765b1822eae5660ca5497e233d92d
dzozrawr <jovan_96@live.ru>
2023-06-20 15:09:20 +0200

    added currency system and removing item from shop when bought

--- Assets/_Game/Scripts/GameController.cs
+++ Assets/_Game/Scripts/GameController.cs
@@ -7,8 +7,12 @@ public class GameController : MonoBehaviour
     private static GameController instance = null;
 
     public static GameController Instance { get => instance; }
-
+    private static int coinAmount;
+    public static int CoinAmount { get => coinAmount; set => coinAmount = value; }
     public PlayerMovement player = null;
+    public delegate void GameControllerEvent();
+
+    public GameControllerEvent MoneyAmountChanged;
 
     private void Awake()
     {
@@ -18,13 +22,20 @@ public class GameController : MonoBehaviour
             return;
         }
         instance = this;
+
+        CoinAmount = 20;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
-
+    public void AddMoney(int moneyAmountToAdd)
+    {
+        coinAmount += moneyAmountToAdd;
+        //if ((moneyEarnedInLevel + moneyAmountToAdd) < 0) moneyEarnedInLevel = 0; else moneyEarnedInLevel += moneyAmountToAdd;
+        MoneyAmountChanged?.Invoke();
+    }
 
 }
--- Assets/_Game/Scripts/ShopItem.cs
+++ Assets/_Game/Scripts/ShopItem.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+//using text
 
 [RequireComponent(typeof(Button))]
 public class ShopItem : MonoBehaviour
 {
     public GameObject itemPrefab = null;
+    public int price;
+    public TMP_Text priceText = null;
 
     private Button button = null;
 
@@ -16,6 +20,8 @@ public class ShopItem : MonoBehaviour
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(OnClick);
+
+        priceText.text = price + "";
     }
 
     private void Start()
@@ -24,6 +30,17 @@ public class ShopItem : MonoBehaviour
     }
     private void OnClick()
     {
-        gameController.player.AddClothing(itemPrefab);
+        if (GameController.CoinAmount >= price)
+        {
+            gameController.player.AddClothing(itemPrefab);
+            gameObject.SetActive(false);//remove it from shop
+            //play buying Sound effect
+            gameController.AddMoney(-price);    //subtract money
+        }
+        else
+        {
+            //play error sound effect maybe
+        }
+
     }
 }
